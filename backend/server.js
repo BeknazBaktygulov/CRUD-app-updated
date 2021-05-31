07e9c2b6f023dc24e9b0b2e6e20b1311b61242b0
Beknaz Baktygulov <enelbbs9@gmail.com>
2021-05-31 16:55:05 -0700

    Added db connection explanation

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,8 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+
+// Brought in MongoDB
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true }
 );
